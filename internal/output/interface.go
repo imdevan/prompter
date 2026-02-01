75f3fd02ca39c7f6eddedbf38158ba231a5d0981
Devan Huapaya <huapayadevan@gmail.com>
2026-01-31 21:20:54 -0800

    add clipboard and editor interface

--- internal/output/interface.go
+++ internal/output/interface.go
@@ -1,6 +1,11 @@
 package output
 
-// Destination describes an output target.
-type Destination interface {
-	Write(content string) error
+// Clipboard writes content to the system clipboard.
+type Clipboard interface {
+	WriteText(text string) error
+}
+
+// Editor opens a file in the user's editor.
+type Editor interface {
+	Open(path string) error
 }
