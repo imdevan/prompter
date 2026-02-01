cbbd95926eb6fa5cc250d479bc8d14595e3f7215
Devan Huapaya <huapayadevan@gmail.com>
2026-01-31 21:18:55 -0800

    implement clipboard adapter

--- internal/adapters/clipboard/clipboard.go
+++ internal/adapters/clipboard/clipboard.go
@@ -1,3 +1,11 @@
 package clipboard
 
-// TODO: implement clipboard adapter.
+import atotto "github.com/atotto/clipboard"
+
+// Adapter writes content to the system clipboard.
+type Adapter struct{}
+
+// WriteText copies text to the clipboard.
+func (Adapter) WriteText(text string) error {
+	return atotto.WriteAll(text)
+}
