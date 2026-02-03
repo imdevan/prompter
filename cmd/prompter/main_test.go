818ac6f22deb548d50cede60534cf3b3a7bf542a
Devan Huapaya <huapayadevan@gmail.com>
2026-02-03 15:06:50 -0800

    update force all tests to non-interactive

--- cmd/prompter/main_test.go
+++ cmd/prompter/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"os/exec"
 	"testing"
 )
@@ -8,7 +9,9 @@ import (
 func TestPrompterRuns(t *testing.T) {
 	t.Parallel()
 
-	cmd := exec.Command("go", "run", ".")
+	cacheDir := t.TempDir()
+	cmd := exec.Command("go", "run", ".", "--yes", "--target=stdout")
+	cmd.Env = append(os.Environ(), "XDG_CACHE_HOME="+cacheDir, "NO_COLOR=1")
 	if output, err := cmd.CombinedOutput(); err != nil {
 		t.Fatalf("prompter failed to run: %v\n%s", err, string(output))
 	}
