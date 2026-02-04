7aebca806fb5911f3b8da4fe8682bf3a1752ca90
Devan Huapaya <huapayadevan@gmail.com>
2026-02-03 18:23:47 -0800

    add in-process command runner for tests

--- cmd/prompter/main_test.go
+++ cmd/prompter/main_test.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"os"
-	"os/exec"
 	"testing"
+
+	"prompter-cli/internal/testutil"
 )
 
 func TestPrompterRuns(t *testing.T) {
 	t.Parallel()
 
-	cacheDir := t.TempDir()
-	cmd := exec.Command("go", "run", ".", "--yes", "--target=stdout")
-	cmd.Env = append(os.Environ(), "XDG_CACHE_HOME="+cacheDir, "NO_COLOR=1")
-	if output, err := cmd.CombinedOutput(); err != nil {
-		t.Fatalf("prompter failed to run: %v\n%s", err, string(output))
+	testutil.WithTempXDG(t)
+	cmd := newRootCmd()
+	output, err := testutil.RunCLI(t, cmd, "--yes", "--target=stdout")
+	if err != nil {
+		t.Fatalf("prompter failed to run: %v\n%s", err, output)
 	}
 }
