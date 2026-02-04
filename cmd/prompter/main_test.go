d4c9b4f7f8148e68eeacfb8811ad639a0a6e8e18
Devan Huapaya <huapayadevan@gmail.com>
2026-02-03 21:13:35 -0800

    update gate integraiton tests

--- cmd/prompter/main_test.go
+++ cmd/prompter/main_test.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"prompter-cli/internal/testutil"
 )
 
 func TestPrompterRuns(t *testing.T) {
+	if testing.Short() || os.Getenv("TEST_INTEGRATION") == "" {
+		t.Skip("skipping integration test; set TEST_INTEGRATION=1 to enable")
+	}
 	testutil.WithTempXDG(t)
 	cmd := newRootCmd()
 	output, err := testutil.RunCLI(t, cmd, "--yes", "--target=stdout")
